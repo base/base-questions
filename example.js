146995e93110897cabe4730c4dad55f8a6f81644
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-29 16:16:36 -0500

    update examples

---
+++ example.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var questions = require('./');
+var assemble = require('assemble-core');
+var store = require('base-store');
+var argv = require('base-argv');
+
+var app = assemble();
+app.use(store());
+app.use(argv());
+
+var argv = app.argv(process.argv.slice(2));
+app.use(questions(app, argv.options));
+
+app.task('ask', function (cb) {
+  app.ask(function (err, answers) {
+    if (err) return cb(err);
+    console.log(answers);
+    cb();
+  });
+});
+
+app.task('a', function (cb) {
+  console.log('task > a!');
+  cb();
+});
+
+app.task('b', function (cb) {
+  console.log('task > b!');
+  cb();
+});
+
+app.task('c', function (cb) {
+  console.log('task > c!');
+  cb();
+});
+
+app.task('choices', function (cb) {
+  app.choices('run', ['a', 'b', 'c'], function (err, answers) {
+    if (err) return cb(err);
+    if (!answers.run.length) return cb();
+    app.build(answers.run, cb);
+  });
+});
+
+app.build('choices', function(err) {
+  if (err) return console.log(err);
+  console.log('done!');
+});
