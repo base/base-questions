c13d310d6ef76394160e1a630b0d344a0f7d184c
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-01 17:01:58 -0500

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,21 @@ var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
 
-var lint = ['gulpfile.js', 'index.js', 'lib/*.js', 'test/*.js'];
-
 gulp.task('coverage', function() {
-  return gulp.src(lint)
+  return gulp.src(['index.js', 'utils.js'])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
 gulp.task('mocha', ['coverage'], function() {
-  return gulp.src('test/*.js')
+  return gulp.src('test.js')
     .pipe(mocha({reporter: 'spec'}))
-    .pipe(istanbul.writeReports());
+    .on('error', console.error)
+    .pipe(istanbul.writeReports())
 });
 
 gulp.task('eslint', function() {
-  return gulp.src(lint)
+  return gulp.src(['index.js', 'gulpfile.js', 'utils.js', 'test.js'])
     .pipe(eslint())
 });
 
